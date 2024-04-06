a1173a6f5027b26d664c64c629e457874602baaf
Ashu Sharma <ashusharma.sigmadev@gmail.com>
2024-04-06 18:05:44 +0530

    initial

---
+++ README.md
@@ -0,0 +1,41 @@
+# Auto Project Starter (.start)
+
+This is a Visual Studio Code (VSCode) extension that automatically starts your project by running commands specified in a `.start` file in the project root folder.
+
+## Features
+
+- Automatically creates new terminals and runs the commands specified in the `.start` file whenever you run the `Boot .start` command.
+- Supports multiple commands separated by commas, with each command running in a new terminal.
+- Provides an easy way to set up and start your project without having to manually run the same commands every time.
+
+## Installation
+
+1. Open the VSCode Extension Marketplace by pressing `Ctrl+Shift+X` (Windows/Linux) or `Cmd+Shift+X` (Mac).
+2. Search for "Auto Project Starter" and click on the "Install" button.
+3. Once the extension is installed, restart VSCode.
+
+## Usage
+
+1. Create a `.start` file in the root folder of your project.
+2. In the `.start` file, add the commands you want to run, with each command on a new line, prefixed with `\t`. For example:
+3. Open the project in VSCode, and press `Ctrl+Shift+X` (Windows/Linux) or `Cmd+Shift+X` (Mac) and then choose `Boot .start` command to start it.
+
+## Example `.start` file
+
+```bash
+\t python -m venv venv
+\t source venv/bin/activate, pip install -r requirements.txt
+\t python app.py
+```
+
+## Configuration
+
+This extension does not have any configurable options at the moment.
+
+## Feedback and Contributions
+
+If you have any feedback, suggestions, or want to contribute to the development of this extension, please visit the [GitHub repository](https://github.com/Equibre/.start.git).
+
+## License
+
+This extension is licensed under the [MIT License](LICENSE).
---
+++ extension.js
@@ -0,0 +1,79 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+const START_FILE_NAME = ".start";
+
+function executeCommands(commands) {
+  for (const commandGroup of commands) {
+    const terminal = vscode.window.createTerminal();
+    for (const command of commandGroup) {
+      terminal.sendText(command);
+    }
+    terminal.show();
+  }
+}
+
+function parseStartFile(rootFolder) {
+  const startFilePath = path.join(rootFolder, START_FILE_NAME);
+  fs.readFile(startFilePath, "utf8", (err, data) => {
+    if (err) {
+      vscode.window.showErrorMessage(
+        "Can't locate .start file in project root."
+      );
+      return;
+    }
+
+    const commands = data
+      .split("\n")
+      .filter((line) => line.startsWith("\\t"))
+      .map((line) =>
+        line
+          .slice(3)
+          .split(",")
+          .map((cmd) => cmd.trim())
+      );
+
+    executeCommands(commands);
+  });
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  let disposable = vscode.commands.registerCommand(
+    "start.bootProject",
+    function () {
+      const activeEditor = vscode.window.activeTextEditor;
+      if (activeEditor) {
+        parseStartFile(path.dirname(activeEditor.document.uri.fsPath));
+      } else {
+        vscode.window.showInformationMessage("No active editor found.");
+      }
+    }
+  );
+
+  // context.subscriptions.push(
+  //   vscode.workspace.onDidChangeWorkspaceFolders((event) => {
+  //     if (event.added.length > 0) {
+  //       parseStartFile(event.added[0].uri.fsPath);
+  //     }
+  //   })
+  // );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
